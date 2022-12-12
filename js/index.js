26c446a4e4d14226cfb9f933eb2e579377668d16
CustomFont <jjddff12@gmail.com>
2022-12-12 17:00:38 -0500

    end day1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,85 @@
+let 
 const rootUrl = 'https://swapi.dev/api/'
-let posts = fetch(rootUrl)
+// get all root catagories
+let root = fetch(rootUrl)
   .then(function(response) {
     return response.json()
    })
   .then(function(response){
-    console.log(response)
+    //console.log(response)
     return Object.entries(response)
-  })
+    })
+  
+  // get all people
+let getAllPeople = fetch('https://swapi.dev/api/people/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+  // get all planets
+let getAllPlanets = fetch('https://swapi.dev/api/planets/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+  // get all films
+let getAllFilms = fetch('https://swapi.dev/api/films/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+  // get all species
+let getAllSpecies = fetch('https://swapi.dev/api/species/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+  // get all spaceships
+let getAllStarships = fetch('https://swapi.dev/api/starships/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+  // get all vehicles
+let getAllVehicles = fetch('https://swapi.dev/api/vehicles/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        //console.log(response)
+        return Object.entries(response)
+    })
+
+// let getVehicleByID = fetch()
+
+
+
+
+//--------------------------------------
+let setItemID = function() {
+    
+}
+let renderInfo = function(itemID) {
+
+
+}
